b31cb327b908bc8f7883bd133885e0ebeca9b963
rickjp <3741326+RickJP@users.noreply.github.com>
2020-09-27 18:41:48 +0900

    initial commit

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
+
+import Dropdown from './dropdown';
+import data from '../data/animals.json';
 
 export default function App() {
-  return <div>Start here</div>;
+  const [value, setValue] = useState(null);
+
+  return (
+    <div style={{ width: 200 }}>
+      <Dropdown
+        prompt='Select animal...'
+        options={data}
+        id='id'
+        label='name'
+        value={value}
+        onChange={(val) => setValue(val)}
+      />
+    </div>
+  );
 }
